e5db0be55c19d99fa72d70806e333cfb42e1dabf
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-08-02 06:09:23 +0545

    Home,Signup and login page created with routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from "react";
-import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../features/navbar/Navbar";
 import ProductList from "../features/product-list/ProductList";
 
-const Home = () => {
+const HomePage = () => {
   return (
     <div>
       <Navbar>
@@ -12,4 +12,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default HomePage;
